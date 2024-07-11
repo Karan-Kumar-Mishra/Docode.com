3fe4617b4e0b7ba85ee023b0fd538e8a60337f09
KaranMishra <93134411+Karan-Kumar-Mishra@users.noreply.github.com>
2024-07-11 21:56:31 +0530

    Add files via upload

---
+++ Online-compiler/script.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/Karan-Kumar-Mishra/Online-compiler
+cd Online-compiler
+cd Online-compiler
+npm install
+apt install gcc -y
+apt install python3 -y
+npm install
+node server.js
