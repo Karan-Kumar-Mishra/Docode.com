8ea3e809c739bf164331f28b90000a5e479cf7bb
KaranMishra <93134411+Karan-Kumar-Mishra@users.noreply.github.com>
2024-07-18 12:06:39 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/Karan-Kumar-Mishra/Online-compiler
-cd Online-compiler
-cd Online-compiler
-npm install
-apt install gcc -y
-apt install python3 -y
-npm install
-node server.js
